dee30113aed0d40c307fef4daa810e40b958c01e
Michael Nowlin <michaelnowlin5@gmail.com>
2022-05-07 19:50:37 -0600

    finished unit test. on to automated tests

--- functions.test.ts
+++ functions.test.ts
@@ -1,5 +1,16 @@
 const {shuffleArray} = require('./utils')
-
+const testArray = [1,2,3,4];
 describe('shuffleArray should', () => {
-    // CODE HERE
+    test('return anything not null.', () => {
+        expect(shuffleArray([1,2,3,4])).toBeTruthy();
+    })
+    test('have the same amount in the array', () => {
+        expect(shuffleArray(testArray)).toHaveLength(testArray.length);
+    })
+    test('have the number i put in it.', () => {
+        expect(shuffleArray(testArray)).toContain(1);
+        expect(shuffleArray(testArray)).toContain(2);
+        expect(shuffleArray(testArray)).toContain(3);
+        expect(shuffleArray(testArray)).toContain(4);
+    })
 })
