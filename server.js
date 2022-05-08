54b3db0ec0f71600ec9e1a4017bc3fbe4163a241
Michael Nowlin <michaelnowlin5@gmail.com>
2022-05-07 21:19:10 -0600

    trying to fix why my website doent want to deploy

--- server.js
+++ server.js
@@ -1,3 +1,7 @@
+require('dotenv').config();
+
+const { PORT} = process.env;
+
 const express = require('express')
 const path = require('path')
 const app = express()
@@ -93,8 +97,8 @@ app.get('/api/player', (req, res) => {
 
 
 
-const port = process.env.PORT || 3000
+//const port = process.env.PORT || 3000
 
-app.listen(port, () => {
-  console.log(`Listening on port ${port}`)
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`)
 })
